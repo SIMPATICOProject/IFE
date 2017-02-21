e5bb8d03f8245247b87deee97575c13afad2913f
Enrique Sanz <enrique.sanz@deusto.es>
2017-02-21 10:30:51 +0100

    Some comments and documentation

---
+++ js/ctz-core.js
@@ -0,0 +1,12 @@
+// CITIZENPEDIA
+// Citizepedia Calls and methods
+
+// Get questions from Citizenpedia. Need the eservice code and the paragraph or sentence id
+function getQuestions(simpaticoEservice,name,questionsCallback)
+{
+  jQuery.getJSON(baseURL+'/citizenpedia/api/qae/questions/'+simpaticoEservice+'/'+name,
+    function(jsonResponse)
+    {
+      questionsCallback(name,jsonResponse);
+    });
+}
