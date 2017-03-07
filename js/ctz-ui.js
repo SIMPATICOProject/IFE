3113497664bb4e0e57431c3a3340e07bc701a8d9
Ivan Pretel <ivan.pretel@deusto.es>
2017-03-07 17:31:21 +0100

    Added pattern and code clean in ctz-ui.js

--- js/ctz-ui.js
+++ js/ctz-ui.js
@@ -3,142 +3,168 @@
 // This JavaScript contains the functionality related to the User Interface
 // which enriches the Interactive Front-End component with the features of 
 // the Citizenpedia component.
-// - It uses the methods implemented in ctz-core.js.
-// - All this methods are related to the feature/button named 'citizenpedia' 
-//   in the buttons variable (see simpatico-ife.js code)
-// - IMPORTANT: The citizenpediaURL variable should be declared.
-//      Example: https://simpatico.morelab.deusto.es/citizenpedia
+// - It uses the methods implemented in ctz-core.js
+// - The Citizenpedia server side code is available in:
+//              https://github.com/SIMPATICOProject/Citizenpedia
 //-----------------------------------------------------------------------------
 
-// This is the main URL of the used Citizenpedia instance 
-var citizenpediaURL = 'https://my-citizenpedia-instace.com';
+var citizenpediaUI = (function () {
+  var instance; // Singleton Instance of the UI component
+  var featureEnabled = false;
+  function Singleton () {
+    // Component-related variables
+    var primaryColor = '';
+    var secondaryColor = '';
+    var questionsBoxTitle = '';
+    var addQuestionLabel = '';
 
-// Label for the "Related Questions" box
-var questionsBoxTitle = "RELATED QUESTIONS";
-var addQuestionLabel = "+ Add new question";
-var questionListBackgroundColor = "#D3F2F8";
+    // Internal usage variables
+    var paragraphs = []; // Used to store all the tagged paragraphs
 
-var paragraphs =[];
-var questionsHtml;
-var isCitizenpediaEnabled = false;
+    // Component-related methods and behaviour
+    function initComponent(parameters) {
+      primaryColor = parameters.primaryColor;
+      secondaryColor = parameters.secondaryColor;
+      questionsBoxTitle = parameters.questionsBoxTitle;
+      addQuestionLabel = parameters.addQuestionLabel;
+      qaeCORE.getInstance().init({
+          endpoint: parameters.endpoint
+        });
+    }
+    
+    function enableComponentFeatures() {
+      if (featureEnabled) return;
+      featureEnabled = true;
 
+      // Gets the tagged paragraphs the first time
+      if (paragraphs.length === 0) {
+        paragraphs = document.getElementsByClassName("simp-text-paragraph");
+      }
 
-// This is the main function called by the corresponding button of the toolbar
-// It enables or disables the Citizenpedia feature
-function switchcitizenpedia()
-{
-  if (!isCitizenpediaEnabled) {
-    enableCitizenpedia();
-    isCitizenpediaEnabled = true;
-  } else {
-    disableCitizenpedia();
-    isCitizenpediaEnabled = false;
-  }
-}
+      // Add special format and add a couple of attributes to the paragraphs
+      var paragrapId = 1;
+      var paragraphName = '';
+      for (var i = 0, len = paragraphs.length; i < len; i++) {
+        paragraphName = "Paragraph" + paragrapId;
+        paragraphs[i].style.position = 'relative';
+        paragraphs[i].style.borderLeft = "thick solid " + primaryColor;
+        paragraphs[i].setAttribute("id", paragraphName);
+        paragraphs[i].setAttribute("onclick", 
+          "citizenpediaUI.getInstance()." + 
+          "paragraphEvent('" + paragraphName + "');");
+        paragrapId++;
+      }
+    }
+  
+    function disableComponentFeatures() {
+      if (!featureEnabled) return;
+      featureEnabled = false;
 
-// It gets the tagged paragraphs and enhances them with the Citizenpedia features 
-function enableCitizenpedia()
-{
-  functionColor = getFunctionColor("citizenpedia");
-  functionColor = "#24BCDA"
+      // Remove Question Boxes
+      var questionDivs = document.getElementsByClassName("citizenpedia_questions");
+      for (var i = questionDivs.length - 1; i >= 0; i--) {
+        questionDivs[i].parentNode.removeChild(questionDivs[i]);
+      }
+      
+      // Reformat the paragraphs
+      for (var i = 0, len = paragraphs.length; i < len; i++) {
+        paragraphs[i].style.borderLeft = "none";
+        paragraphs[i].removeAttribute("onclick");
+      }
+    }
 
-  // 
-  if (document.getElementById('citizenpediaSwitch').value == "citizenpediaOn") {
-      switchFunction("citizenpedia");
-  }
+    // If the Component feature is enabled it calls to the Citizenpedia instance to 
+    // get the questions related to the paragraph passed as parameter
+    // - paragraphName: the id of the paragraph which has produced the event
+    // IMPORTANT: Here is used the golbal variable simpaticoEservice
+    function paragraphEvent(paragraphName) {
+      if (!featureEnabled) return;
+      if (document.getElementById(paragraphName + "_questions") === null) {
+        qaeCORE.getInstance().getQuestions(simpaticoEservice, paragraphName, drawQuestionsBox);
+      } else {
+        hideQuestionsBox(paragraphName);
+      }
+    }
+    
+    // Draw the questions box
+    // - paragraphName: the id of the paragraph
+    // - responseQuestions: the JSON Object of the questions related to the paragraph
+    function drawQuestionsBox(paragraphName, responseQuestions) {
+      // Create questions div
+      var questionsDiv = document.createElement('div');
+      questionsDiv.id = paragraphName + "_questions";
+      questionsDiv.className = "citizenpedia_questions";
+      questionsDiv.style.borderLeft = "thick solid " + primaryColor;
+      questionsDiv.style.borderTop = "thick solid " + primaryColor;
+      questionsDiv.style.backgroundColor = secondaryColor;
+      
+      // 1. the title is attached 
+      questionsHtml = '<p ' +   
+                      'style="font-weight: bold; ' +
+                        'color: WHITE; ' +
+                        'background-color:' + primaryColor + '; ' +
+                        'margin-left:0px; ' +
+                        'margin-right:0px"' +    
+                      'id="ctz-ui-qb-title">' +    
+                      questionsBoxTitle +
+                      '</p>';
 
-  // Gets the tagged paragraphs the first time
-  if (paragraphs.length === 0) {
-    paragraphs = document.getElementsByClassName("simp-text-paragraph");
-  }
+      // 2. A list containing the made questions is attached
+      questionsHtml += '<ul>';
 
-  // Add special format to the paragraphs
-  var paragrapId = 1;
-  for (var i = 0, len = paragraphs.length; i < len; i++) {
-    paragraphs[i].setAttribute("id", "Paragraph" + paragrapId);
-    var paragraph = document.getElementById(paragraphs[i].id);
-    var paragraphName = paragraphs[i].id;
-    paragraphs[i].style.position = 'relative';
-    paragraphs[i].setAttribute("onclick", "citizenpedia('" + paragraphName + "');");
-    paragraphs[i].style.borderLeft = "thick solid " + functionColor;
-    paragrapId++;
-  }
-}
+      // 2.a. For each question a new bulletpoint is made 
+      for (var i = 0, len = responseQuestions.length; i < len; i++) {
+        questionsHtml += '<li onclick="cancelClick(event);">' + 
+                            '<a href="' + 
+                                qaeCORE.getInstance().createQuestionDetailsURL(
+                                  responseQuestions[i]._id) + 
+                                '">' + responseQuestions[i].title + 
+                            '</a>' +
+                         '</li>';
+      }
 
-// It removes the Citizenpedia features of the tagged paragraphs
-function disableCitizenpedia()
-{
-  // Remove Question Boxes
-  var questionDivs = document.getElementsByClassName("citizenpedia_questions");
-  for (var i = questionDivs.length - 1; i >= 0; i--) {
-    questionDivs[i].parentNode.removeChild(questionDivs[i]);
+console.log("NAME=" + paragraphName + "");
+
+      // 2.b. Finally the Add Question link is also attached 
+      questionsHtml += '<li onclick="cancelClick(event);">'
+      questionsHtml +=    '<a href="' + 
+                                qaeCORE.getInstance().createNewQuestionURL(
+                                  "Benestar", 
+                                  simpaticoEservice,
+                                  paragraphName, 
+                                  document.getElementById(paragraphName).textContent) + 
+                                '">' + addQuestionLabel + 
+                          '</a>'
+      questionsHtml += '</li>';
+      
+      questionsHtml += '</ul>';
+
+      questionsDiv.innerHTML = questionsHtml;
+      document.getElementById(paragraphName).appendChild(questionsDiv);
+    } //drawQuestionsBox
+
+    // Hide the questions box attached to a paragraph passed as paramether
+    // - paragraphName: the id of the paragraph
+    function hideQuestionsBox(paragraphName) {
+      var qBoxToRemove = document.getElementById(paragraphName + "_questions");
+      qBoxToRemove.parentNode.removeChild(qBoxToRemove);
+    }
+
+    return {
+    // Public definitions
+      init: initComponent, // Called only one time
+      enable: enableComponentFeatures,  // Called when the Component button is enabled
+      disable: disableComponentFeatures, // Called when the Component button is disabled or another one enabled
+      isEnabled: function() { return featureEnabled;}, // Returns if the feature is enabled
+    // Public methods shoudl be used 
+      paragraphEvent: paragraphEvent
+    };
   }
   
-  // Reformat the paragraphs
-  for (var i = 0, len = paragraphs.length; i < len; i++) {
-    paragraphs[i].style.borderLeft = "none";
-    paragraphs[i].removeAttribute("onclick");
-  }
-}
-
-
-// Make the call to the Citizenpedia
-// - name: the id of the paragraph
-function citizenpedia(name)
-{
-  if (document.getElementById(name + "_questions") === null) {
-    getQuestions(simpaticoEservice, name, drawQuestionsBox);
-  } else {
-    hideQuestionsBox(name);
-  }
-
-}//citizenpedia
-
-// Draw the questions box
-// - name: the id of the paragraph
-// - responseQuestions: the JSON Object of the questions related to the paragraph
-function drawQuestionsBox(name, responseQuestions)
-{
-
-  // Create questions div
-  var questionsDiv = document.createElement('div');
-  questionsDiv.id = name + "_questions";
-  questionsDiv.className = "citizenpedia_questions";
-  questionsDiv.style.borderLeft = "thick solid " + functionColor;
-  questionsDiv.style.borderTop = "thick solid " + functionColor;
-  questionsDiv.style.backgroundColor = questionListBackgroundColor;
-  
-  // 1. the title is attached 
-  questionsHtml = "<p " +   
-                  "style=\" font-weight: bold; color: WHITE; background-color:" + functionColor + "; margin-left:0px; margin-right:0px \"" +    
-                  "id=\"ctz-ui-qb-title\">" +    
-                  questionsBoxTitle + "</p>";
-
-  // 2. A list containing the made questions is attached
-  questionsHtml += "<ul>";
-
-  // 2.a. For each question a new bulletpoint is made 
-  for (var i = 0, len = responseQuestions.length; i < len; i++) {
-    questionsHtml += "<li onclick=\"cancelClick(event);\">" + 
-                        "<a href=\"" + createQuestionDetailsURL(responseQuestions[i]._id) + "\">" + responseQuestions[i].title + "</a>" +
-                     "</li>";
-  }
-
-  // 2.b. Finally the Add Question link is also attached 
-  questionsHtml += "<li onclick=\"cancelClick(event);\">"
-  questionsHtml +=    "<a href=\"" + createNewQuestionURL("Benestar", simpaticoEservice, name, document.getElementById(name).textContent) + "\">" + addQuestionLabel + "</a>"
-  questionsHtml += "</li>";
-  
-  questionsHtml += "</ul>";
-
-  questionsDiv.innerHTML = questionsHtml;
-  document.getElementById(name).appendChild(questionsDiv);
-} //drawQuestionsBox
-
-// Hide the questions box attached to a paragraph passed as paramether
-// - name: the id of the paragraph
-function hideQuestionsBox(name)
-{
-  var qBoxToRemove = document.getElementById(name + "_questions");
-  qBoxToRemove.parentNode.removeChild(qBoxToRemove);
-}
+  return {
+    getInstance: function() {
+      if(!instance) instance = Singleton();
+      return instance;
+    }
+  };
+})();
