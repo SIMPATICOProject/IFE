42607e3954abea7ca5d123382b8a4abb8ed230dd
Ivan Pretel <ivan.pretel@deusto.es>
2017-04-11 16:10:15 +0200

    Added clean and main structure of log client

---
+++ js/log-ui.js
@@ -0,0 +1,77 @@
+// Citizenpedia User Interface (ctz-ui.js)
+//-----------------------------------------------------------------------------
+// This JavaScript contains the functionality related to the User Interface
+// which enriches the Interactive Front-End component with the features of 
+// the Citizenpedia component.
+// - It uses the methods implemented in ctz-core.js
+// - The Citizenpedia server side code is available in:
+//              https://github.com/SIMPATICOProject/Citizenpedia
+//-----------------------------------------------------------------------------
+
+// Log Core Client (log-core.js)
+//-----------------------------------------------------------------------------
+// This JavaScript contains the functionality related to the User Interface of 
+// the Log component. The main functionality is to hook and log the main events 
+// through the calls to the server side of the corresponding Log instance
+// - Used by ctz-ui.js
+// - The Log server side code is available in:
+//              https://github.com/SIMPATICOProject/logs
+//-----------------------------------------------------------------------------
+
+var logUI = (function () {
+  var instance; // Singleton Instance of the UI component
+  var featureEnabled = true;
+  function Singleton () {
+    // Internal usage variables
+    var paragraphs = []; // Used to store all the tagged paragraphs
+    var originalStyles = []; // Used to store the tagged paragraphs CSSstyles
+    var diagramContainer; // Used to show the CPD diagram
+
+    // Component-related methods and behaviour
+    function initComponent(parameters) {
+      logCORE.getInstance().init({
+          endpoint: parameters.endpoint
+        });
+    }
+    
+    function enableComponentFeatures() {
+      if (featureEnabled) return;
+      featureEnabled = true;
+    }
+  
+    function disableComponentFeatures() {
+      if (!featureEnabled) return;
+      featureEnabled = false;
+    }
+
+
+    // It logs an event caused when a user uses a Simpatico feature.
+    // - component: Component which produces the event (e.g. tae, wae, ctz...)
+    // - element: Id of the element that causes the event (e.g. paragraphID...)
+    // - event: Id of the element that causes the event (e.g. paragraphID...)
+    // - details: Optional parameter to pass additional info if it is required
+    function logSimpaticoEvent(component, element, event, details) {
+      logCORE.getInstance().logSimpaticoEvent(component, element, event, details);
+    }
+
+
+
+    return {
+      // Public definitions
+      init: initComponent, // Called only one time
+      enable: enableComponentFeatures,  // Called when the Component button is enabled
+      disable: disableComponentFeatures, // Called when the Component button is disabled or another one enabled
+      isEnabled: function() { return featureEnabled;}, // Returns if the feature is enabled
+
+      logSimpaticoEvent: logSimpaticoEvent,
+      logTimeEvent: function() { logCORE.getInstance().logTimeEvent}
+    };
+  }
+  
+  return {
+    getInstance: function() {
+      if(!instance) instance = Singleton();
+      return instance;
+    }
+  };
+})();
